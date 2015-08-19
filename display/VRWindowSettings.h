697a2eb5f051185fab6ddf92dd751b71246903d3
Daniel Keefe <keefe@cs.umn.edu>
2015-08-19 10:33:45 -0500

    added doc directory with current UML class diagram and updated several files to reflect that current design

---
+++ display/VRWindowSettings.h
@@ -0,0 +1,13 @@
+#ifndef VRWINDOWSETTINGS_H
+#define VRWINDOWSETTINGS_H
+
+class VRWindowSettings {
+public:
+  bool hasBorder;
+  std::string title;
+  int GPUAffinityID;
+  // etc..
+};
+
+#endif
+
