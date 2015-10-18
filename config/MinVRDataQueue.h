ab14b1ba37a67312fe9063116d7c99a77f433540
tsgouros <tsgouros@gmail.com>
2015-10-17 22:23:05 -0400

    edited queue documentation

--- config/MinVRDataQueue.h
+++ config/MinVRDataQueue.h
@@ -16,45 +16,6 @@
 //
 // Tom Sgouros 10/16/2015
 //
-// To use:
-//
-// Create a queue object, and add values to the store in one of three ways.
-//
-//  1. You can add data with the specialized add functions, such as
-//     addValueInt(), addValueFloat(), etc.  These take a name and a
-//     value and park them in the queue.
-//
-//  2. You can feed some serialized data to one of the addValue()
-//     functions.  This is just string data, that looks something like
-//     this:
-//
-//     <flora type="int">42</flora>
-//
-//     <bob type="container">
-//        <flora type="int">42</flora>
-//        <pi type="float">3.1415926</pi>
-//     </bob>
-//
-//     The type attributes are usually superfluous, since the code can
-//     usually infer the type from the value string.  However, future
-//     implemented data types might make the inference step less
-//     reliable, so bear that in mind.
-//
-//  3. Feed a file containing XML into processXMLFile().
-//
-//  Once a queue has entries, they can be retrieved at your pleasure
-//  with getDatum() or serialize().  The getDatum() method returns the
-//  next MinVRDatum object in the queue, so can be used directly in
-//  your program.  I hate remembering how to spell the static_cast<>()
-//  options, so these are provided as a convenience via a helper class
-//  methods to the pointer objects.  So (int)p->getDatum() gets you an
-//  integer and (double)p->getDatum() gets you a double, so long as
-//  the relevant objects actually contain an integer and double.
-//
-//  There are no other ways to get a datum value.  Your interest
-//  should be in loading MinVRDatum objects into the queue, and
-//  getting them off the other end.
-//
 //
 class MinVRDataQueue {
 private:
