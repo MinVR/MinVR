afb55037be2e1b78f3e85a2abc2872a0269c1b08
tsgouros <tsgouros@gmail.com>
2015-12-12 16:06:44 -0500

    resolving some branch confusion

--- tests/net/client.cpp
+++ tests/net/client.cpp
@@ -1,19 +1,35 @@
 
 #include <net/VRNetClient.h>
-#include <event/VREvent.h>
+#include <config/VRDataIndex.h>
 #include <math/VRMath.h>
 #ifndef WIN32
 #include <unistd.h>
 #endif
 
 int main() {
+  VRDataIndex *index = new VRDataIndex;
+
+  
   VRNetClient client("localhost", "3490");
+
   int i = 0;
   while (1) {
+    
     std::cout << "in draw loop " << i << std::endl;
     
-    std::vector<VREvent> events;
     client.synchronizeInputEventsAcrossAllNodes(events);
+    VRDataQueue *events = new VRDataQueue( eventData ); 
+
+    while (events->notEmpty()) {
+
+      std::string p = index->addSerializedValue(events->getSerializeObject());
+
+      // examine the data
+
+      std::cout << p->getType() << std::endl;
+      if (p->getType() == VCORETYPE_CONTAINER) 
+      
+    
     for (std::vector<VREvent>::iterator it=events.begin(); it<events.end(); ++it) {
       std::cout << it->toXML() << std::endl;
     }
