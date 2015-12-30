f37a8fc9d370ae24c6dda975dcfa988e30414585
Dan Orban <dtorban@umn.edu>
2015-12-29 19:20:18 -0600

    Added VRPluginInterface and VRDisplayDevice.

--- display/VRDisplayDevice.h
+++ display/VRDisplayDevice.h
@@ -1,28 +1,28 @@
 #ifndef DISPLAYDEVICE_H
 #define DISPLAYDEVICE_H
 
+#include "config/VRDataIndex.h"
 #include <vector>
-#include "VREvent.h"
-#include "VRRenderState.h"
-
 
 /** DisplayDevice:
     An as-simple-as-possible public interface for Display Devices
  */
-class DisplayDevice {
+class VRDisplayDevice {
 public:
+	virtual ~VRDisplayDevice() {}
 
-  /// The typical use for this function is to update display settings (e.g., projection matrix) based on head tracking input
-  virtual void handleUserInput(const std::vector<VREvent> &inputEvents) {}
-
-  virtual int getNumRenderingPasses() { return 1; }
-  virtual void startRenderingPass(int passNum, VRRenderState &state) {}
-  virtual void endRenderingPass(int passNum, VRRenderState &state) {}
-
-  virtual void swapBuffers() {}
+	virtual void use() = 0;
+	virtual void release() = 0;
+	virtual void startRendering() = 0;
+	virtual void finishRendering() = 0;
 };
 
+class VRDisplayDeviceFactory {
+public:
+	virtual ~VRDisplayDeviceFactory() {}
 
+	virtual std::vector<VRDisplayDevice*> create(const VRDataIndex& config) = 0;
+};
 
 
 #endif
---
+++ main/VRPluginInterface.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright Regents of the University of Minnesota, 2015.  This software is released under the following license: http://opensource.org/licenses/GPL-2.0
+ * Source code originally developed at the University of Minnesota Interactive Visualization Lab (http://ivlab.cs.umn.edu).
+ *
+ * Code author(s):
+ * 		Dan Orban (dtorban)
+ */
+
+#ifndef VRPLUGININTERFACE_H_
+#define VRPLUGININTERFACE_H_
+
+#include "display/VRDisplayDevice.h"
+#include "plugin/PluginInterface.h"
+
+namespace MinVR {
+
+class VRPluginInterface : public PluginInterface {
+public:
+	virtual ~VRPluginInterface() {}
+
+	virtual void addVRDisplayDeviceFactory(VRDisplayDeviceFactory* factory) = 0;
+
+	std::string getName() { return "VRPluginInterface"; }
+	const std::type_info& getType() { return typeid(VRPluginInterface); }
+	int getMinVersion() { return getVersion(); }
+	static int getVersion() { return 0; }
+};
+
+} /* namespace MinVR */
+
+#endif /* VRPLUGININTERFACE_H_ */
