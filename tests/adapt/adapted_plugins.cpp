afc3a7393eff0b6350b4ab86eca43b9dbb8b219b
Dan Orban <dtorban@umn.edu>
2016-01-09 04:22:41 -0600

    Added back in keyboard manipulation to test.

--- tests/adapt/adapted_plugins.cpp
+++ tests/adapt/adapted_plugins.cpp
@@ -36,31 +36,31 @@ bool isRunning = true;
  * Main functionality
  */
 int main(int argc, char **argv) {
-  cout << "Registering plugins..." << endl;
-  cout << "Plugin path: " << PLUGINPATH << endl;
+	cout << "Registering plugins..." << endl;
+	cout << "Plugin path: " << PLUGINPATH << endl;
 
-  MVR = new VRMain();
-  if (argc > 1) {
-	  MVR->initialize(argv[1],argv[2]);
-  }
-  MVR->registerEventCallback(&eventCB);
+	MVR = new VRMain();
+	if (argc > 1) {
+		MVR->initialize(argv[1],argv[2]);
+	}
+	MVR->registerEventCallback(&eventCB);
 
-  VRDisplayDevice* display = MVR->getDisplay();
+	VRDisplayDevice* display = MVR->getDisplay();
 
-  display->use(initGL);
+	display->use(initGL);
 
-  // Loop until escape key is hit or main display is closed
-  while (display->isOpen() && isRunning)
-  {
-	  MVR->synchronizeAndProcessEvents();
+	// Loop until escape key is hit or main display is closed
+	while (display->isOpen() && isRunning)
+	{
+		MVR->synchronizeAndProcessEvents();
 
-	  // Render the scene on all displays (passing render function into display)
-	  display->startRendering(render);
-	  MVR->renderEverywhere();
-	  display->finishRendering();
-  }
+		// Render the scene on all displays (passing render function into display)
+		display->startRendering(render);
+		MVR->renderEverywhere();
+		display->finishRendering();
+	}
 
-  delete MVR;
+	delete MVR;
 }
 
 
@@ -76,19 +76,19 @@ double vertAngle = 0.0;
 
 /* Initialize OpenGL Graphics */
 void initGL() {
-  //glutInitDisplayMode (GLUT_DEPTH);
-  // Set background color to black and opaque
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  // Set background depth to farthest
-  glClearDepth(1.0f);
-  // Enable depth testing for z-culling
-  glEnable(GL_DEPTH_TEST);
-  // Set the type of depth-test
-  glDepthFunc(GL_LEQUAL);
-  // Enable smooth shading
-  glShadeModel(GL_SMOOTH);
-  // Nice perspective corrections
-  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	//glutInitDisplayMode (GLUT_DEPTH);
+	// Set background color to black and opaque
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Set background depth to farthest
+	glClearDepth(1.0f);
+	// Enable depth testing for z-culling
+	glEnable(GL_DEPTH_TEST);
+	// Set the type of depth-test
+	glDepthFunc(GL_LEQUAL);
+	// Enable smooth shading
+	glShadeModel(GL_SMOOTH);
+	// Nice perspective corrections
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
 void render(VRRenderState& state) {
@@ -97,190 +97,190 @@ void render(VRRenderState& state) {
 	GLfloat width = state.display->getWidth();
 	GLfloat height = state.display->getHeight();
 
-   // Compute aspect ratio of the new window
-   if (height == 0) height = 1;                // To prevent divide by 0
-   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+	// Compute aspect ratio of the new window
+	if (height == 0) height = 1;                // To prevent divide by 0
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
-   // Set the viewport to cover the new window
-   //glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	// Set the viewport to cover the new window
+	//glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 
-   // Set the aspect ratio of the clipping volume to match the viewport
-   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-   glLoadIdentity();             // Reset
+	// Set the aspect ratio of the clipping volume to match the viewport
+	glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+	glLoadIdentity();             // Reset
 
-   // Enable perspective projection with fovy, aspect, zNear and zFar
-   gluPerspective(1.6*45.0f, aspect, 0.1f, 100.0f);
+	// Enable perspective projection with fovy, aspect, zNear and zFar
+	gluPerspective(1.6*45.0f, aspect, 0.1f, 100.0f);
 
-   //The .m is the GLfloat* you are accessing
-   //glMultMatrix( GLKMatrix4MakePerspective(45.0f, aspect, 0.1f,100.0f ).m );
+	//The .m is the GLfloat* you are accessing
+	//glMultMatrix( GLKMatrix4MakePerspective(45.0f, aspect, 0.1f,100.0f ).m );
 
 	// Clear color and depth buffers
-	   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	   // Enable depth testing for z-culling
-	   glEnable(GL_DEPTH_TEST);
-	   // Set the type of depth-test
-	   glDepthFunc(GL_LEQUAL);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Enable depth testing for z-culling
+	glEnable(GL_DEPTH_TEST);
+	// Set the type of depth-test
+	glDepthFunc(GL_LEQUAL);
 
-	   glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
 
-	   // Render a color-cube consisting of 6 quads with different colors
-	   glLoadIdentity();                 // Reset the model-view matrix
+	// Render a color-cube consisting of 6 quads with different colors
+	glLoadIdentity();                 // Reset the model-view matrix
 
-	   // The stuff that used to be here has moved to the event handler.
+	// The stuff that used to be here has moved to the event handler.
 
-	   gluLookAt (cameraPos[0], cameraPos[1], cameraPos[2],
-	              targetPos[0], targetPos[1], targetPos[2],
-	              cameraAim[0], cameraAim[1], cameraAim[2]);
+	gluLookAt (cameraPos[0], cameraPos[1], cameraPos[2],
+			targetPos[0], targetPos[1], targetPos[2],
+			cameraAim[0], cameraAim[1], cameraAim[2]);
 
-	   glBegin(GL_LINES);
-	     glColor3f(1.0, 1.0, 0.0);
-	     glVertex3f(8.0, 0.0, 0.0);
-	     glVertex3f(0.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+	glColor3f(1.0, 1.0, 0.0);
+	glVertex3f(8.0, 0.0, 0.0);
+	glVertex3f(0.0, 0.0, 0.0);
 
-	     glColor3f(0.0, 1.0, 1.0);
-	     glVertex3f(0.0, 8.0, 0.0);
-	     glVertex3f(0.0, 0.0, 0.0);
+	glColor3f(0.0, 1.0, 1.0);
+	glVertex3f(0.0, 8.0, 0.0);
+	glVertex3f(0.0, 0.0, 0.0);
 
-	     glColor3f(1.0, 0.0, 1.0);
-	     glVertex3f(0.0, 0.0, 8.0);
-	     glVertex3f(0.0, 0.0, 0.0);
+	glColor3f(1.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, 8.0);
+	glVertex3f(0.0, 0.0, 0.0);
 
-	   glEnd();
-
-
-	   glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
-	      // Top face (y = 1.0f)
-	      // Define vertices in counter-clockwise (CCW) order with normal
-	      // pointing out
-	     glColor3f(0.0f, 0.5f, 0.0f);     // Green
-	     glVertex3f( 1.0f, 1.0f, -1.0f);
-	     glVertex3f(-1.0f, 1.0f, -1.0f);
-	     glVertex3f(-1.0f, 1.0f,  1.0f);
-	     glVertex3f( 1.0f, 1.0f,  1.0f);
-
-	     // Bottom face (y = -1.0f)
-	     glColor3f(0.5f, 0.25f, 0.0f);     // Orange
-	     glVertex3f( 1.0f, -1.0f,  1.0f);
-	     glVertex3f(-1.0f, -1.0f,  1.0f);
-	     glVertex3f(-1.0f, -1.0f, -1.0f);
-	     glVertex3f( 1.0f, -1.0f, -1.0f);
-
-	     // Front face  (z = 1.0f)
-	     glColor3f(0.5f, 0.0f, 0.0f);     // Red
-	     glVertex3f( 1.0f,  1.0f, 1.0f);
-	     glVertex3f(-1.0f,  1.0f, 1.0f);
-	     glVertex3f(-1.0f, -1.0f, 1.0f);
-	     glVertex3f( 1.0f, -1.0f, 1.0f);
-
-	     // Back face (z = -1.0f)
-	     glColor3f(0.5f, 0.5f, 0.0f);     // Yellow
-	     glVertex3f( 1.0f, -1.0f, -1.0f);
-	     glVertex3f(-1.0f, -1.0f, -1.0f);
-	     glVertex3f(-1.0f,  1.0f, -1.0f);
-	     glVertex3f( 1.0f,  1.0f, -1.0f);
-
-	     // Left face (x = -1.0f)
-	     glColor3f(0.0f, 0.0f, 0.5f);     // Blue
-	     glVertex3f(-1.0f,  1.0f,  1.0f);
-	     glVertex3f(-1.0f,  1.0f, -1.0f);
-	     glVertex3f(-1.0f, -1.0f, -1.0f);
-	     glVertex3f(-1.0f, -1.0f,  1.0f);
-
-	     // Right face (x = 1.0f)
-	     glColor3f(0.5f, 0.0f, 0.5f);     // Magenta
-	     glVertex3f(1.0f,  1.0f, -1.0f);
-	     glVertex3f(1.0f,  1.0f,  1.0f);
-	     glVertex3f(1.0f, -1.0f,  1.0f);
-	     glVertex3f(1.0f, -1.0f, -1.0f);
-	   glEnd();  // End of drawing color-cube
+	glEnd();
 
 
+	glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+	// Top face (y = 1.0f)
+	// Define vertices in counter-clockwise (CCW) order with normal
+	// pointing out
+	glColor3f(0.0f, 0.5f, 0.0f);     // Green
+	glVertex3f( 1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f,  1.0f);
+	glVertex3f( 1.0f, 1.0f,  1.0f);
 
-	   glTranslatef(1.5f, 7.0f, 0.0f);  // Move right and into the screen
+	// Bottom face (y = -1.0f)
+	glColor3f(0.5f, 0.25f, 0.0f);     // Orange
+	glVertex3f( 1.0f, -1.0f,  1.0f);
+	glVertex3f(-1.0f, -1.0f,  1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f( 1.0f, -1.0f, -1.0f);
 
-	   glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
-	      // Top face (y = 1.0f)
-	      // Define vertices in counter-clockwise (CCW) order with normal
-	      // pointing out
-	     glColor3f(0.0f, 0.5f, 0.0f);     // Green
-	     glVertex3f( 1.0f, 1.0f, -1.0f);
-	     glVertex3f(-1.0f, 1.0f, -1.0f);
-	     glVertex3f(-1.0f, 1.0f,  1.0f);
-	     glVertex3f( 1.0f, 1.0f,  1.0f);
+	// Front face  (z = 1.0f)
+	glColor3f(0.5f, 0.0f, 0.0f);     // Red
+	glVertex3f( 1.0f,  1.0f, 1.0f);
+	glVertex3f(-1.0f,  1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f( 1.0f, -1.0f, 1.0f);
 
-	     // Bottom face (y = -1.0f)
-	     glColor3f(0.5f, 0.25f, 0.0f);     // Orange
-	     glVertex3f( 1.0f, -1.0f,  1.0f);
-	     glVertex3f(-1.0f, -1.0f,  1.0f);
-	     glVertex3f(-1.0f, -1.0f, -1.0f);
-	     glVertex3f( 1.0f, -1.0f, -1.0f);
+	// Back face (z = -1.0f)
+	glColor3f(0.5f, 0.5f, 0.0f);     // Yellow
+	glVertex3f( 1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f,  1.0f, -1.0f);
+	glVertex3f( 1.0f,  1.0f, -1.0f);
 
-	     // Front face  (z = 1.0f)
-	     glColor3f(0.5f, 0.0f, 0.0f);     // Red
-	     glVertex3f( 1.0f,  1.0f, 1.0f);
-	     glVertex3f(-1.0f,  1.0f, 1.0f);
-	     glVertex3f(-1.0f, -1.0f, 1.0f);
-	     glVertex3f( 1.0f, -1.0f, 1.0f);
+	// Left face (x = -1.0f)
+	glColor3f(0.0f, 0.0f, 0.5f);     // Blue
+	glVertex3f(-1.0f,  1.0f,  1.0f);
+	glVertex3f(-1.0f,  1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f,  1.0f);
 
-	     // Back face (z = -1.0f)
-	     glColor3f(0.5f, 0.5f, 0.0f);     // Yellow
-	     glVertex3f( 1.0f, -1.0f, -1.0f);
-	     glVertex3f(-1.0f, -1.0f, -1.0f);
-	     glVertex3f(-1.0f,  1.0f, -1.0f);
-	     glVertex3f( 1.0f,  1.0f, -1.0f);
+	// Right face (x = 1.0f)
+	glColor3f(0.5f, 0.0f, 0.5f);     // Magenta
+	glVertex3f(1.0f,  1.0f, -1.0f);
+	glVertex3f(1.0f,  1.0f,  1.0f);
+	glVertex3f(1.0f, -1.0f,  1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();  // End of drawing color-cube
 
-	     // Left face (x = -1.0f)
-	     glColor3f(0.0f, 0.0f, 0.5f);     // Blue
-	     glVertex3f(-1.0f,  1.0f,  1.0f);
-	     glVertex3f(-1.0f,  1.0f, -1.0f);
-	     glVertex3f(-1.0f, -1.0f, -1.0f);
-	     glVertex3f(-1.0f, -1.0f,  1.0f);
 
-	     // Right face (x = 1.0f)
-	     glColor3f(0.5f, 0.0f, 0.5f);     // Magenta
-	     glVertex3f(1.0f,  1.0f, -1.0f);
-	     glVertex3f(1.0f,  1.0f,  1.0f);
-	     glVertex3f(1.0f, -1.0f,  1.0f);
-	     glVertex3f(1.0f, -1.0f, -1.0f);
-	   glEnd();  // End of drawing color-cube
 
-	   // Render a pyramid consists of 4 triangles
-	   //   glLoadIdentity();                  // Reset the model-view matrix
-	   glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
+	glTranslatef(1.5f, 7.0f, 0.0f);  // Move right and into the screen
 
-	   glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
-	      // Front
-	      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	      glVertex3f( 0.0f, 1.0f, 0.0f);
-	      glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	      glVertex3f(-1.0f, -1.0f, 1.0f);
-	      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	      glVertex3f(1.0f, -1.0f, 1.0f);
+	glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+	// Top face (y = 1.0f)
+	// Define vertices in counter-clockwise (CCW) order with normal
+	// pointing out
+	glColor3f(0.0f, 0.5f, 0.0f);     // Green
+	glVertex3f( 1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f,  1.0f);
+	glVertex3f( 1.0f, 1.0f,  1.0f);
 
-	      // Right
-	      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	      glVertex3f(0.0f, 1.0f, 0.0f);
-	      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	      glVertex3f(1.0f, -1.0f, 1.0f);
-	      glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	      glVertex3f(1.0f, -1.0f, -1.0f);
+	// Bottom face (y = -1.0f)
+	glColor3f(0.5f, 0.25f, 0.0f);     // Orange
+	glVertex3f( 1.0f, -1.0f,  1.0f);
+	glVertex3f(-1.0f, -1.0f,  1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f( 1.0f, -1.0f, -1.0f);
 
-	      // Back
-	      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	      glVertex3f(0.0f, 1.0f, 0.0f);
-	      glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	      glVertex3f(1.0f, -1.0f, -1.0f);
-	      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	      glVertex3f(-1.0f, -1.0f, -1.0f);
+	// Front face  (z = 1.0f)
+	glColor3f(0.5f, 0.0f, 0.0f);     // Red
+	glVertex3f( 1.0f,  1.0f, 1.0f);
+	glVertex3f(-1.0f,  1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f( 1.0f, -1.0f, 1.0f);
 
-	      // Left
-	      glColor3f(1.0f,0.0f,0.0f);       // Red
-	      glVertex3f( 0.0f, 1.0f, 0.0f);
-	      glColor3f(0.0f,0.0f,1.0f);       // Blue
-	      glVertex3f(-1.0f,-1.0f,-1.0f);
-	      glColor3f(0.0f,1.0f,0.0f);       // Green
-	      glVertex3f(-1.0f,-1.0f, 1.0f);
-	   glEnd();   // Done drawing the pyramid
+	// Back face (z = -1.0f)
+	glColor3f(0.5f, 0.5f, 0.0f);     // Yellow
+	glVertex3f( 1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f,  1.0f, -1.0f);
+	glVertex3f( 1.0f,  1.0f, -1.0f);
+
+	// Left face (x = -1.0f)
+	glColor3f(0.0f, 0.0f, 0.5f);     // Blue
+	glVertex3f(-1.0f,  1.0f,  1.0f);
+	glVertex3f(-1.0f,  1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f,  1.0f);
+
+	// Right face (x = 1.0f)
+	glColor3f(0.5f, 0.0f, 0.5f);     // Magenta
+	glVertex3f(1.0f,  1.0f, -1.0f);
+	glVertex3f(1.0f,  1.0f,  1.0f);
+	glVertex3f(1.0f, -1.0f,  1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();  // End of drawing color-cube
+
+	// Render a pyramid consists of 4 triangles
+	//   glLoadIdentity();                  // Reset the model-view matrix
+	glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
+
+	glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
+	// Front
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f( 0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(1.0f, -1.0f, 1.0f);
+
+	// Right
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	// Back
+	glColor3f(1.0f, 0.0f, 0.0f);     // Red
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);     // Green
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+
+	// Left
+	glColor3f(1.0f,0.0f,0.0f);       // Red
+	glVertex3f( 0.0f, 1.0f, 0.0f);
+	glColor3f(0.0f,0.0f,1.0f);       // Blue
+	glVertex3f(-1.0f,-1.0f,-1.0f);
+	glColor3f(0.0f,1.0f,0.0f);       // Green
+	glVertex3f(-1.0f,-1.0f, 1.0f);
+	glEnd();   // Done drawing the pyramid
 }
 
 
@@ -289,8 +289,8 @@ void render(VRRenderState& state) {
 // the other processes.
 void swapCB() {
 
-   // Swap the front and back frame buffers (double buffering)
-   //glutSwapBuffers();
+	// Swap the front and back frame buffers (double buffering)
+	//glutSwapBuffers();
 }
 
 
@@ -300,10 +300,6 @@ void swapCB() {
 // the dataIndex where you can find that event's data.
 void eventCB(const std::string &eventName, VRDataIndex *dataIndex) {
 
-  // The event can be examined here.
-  //std::cout << std::endl << "examining the data..." << std::endl;
-  //dataIndex->printStructure(eventName);
-
 	// If escape is pressed, exit program
 	if (eventName == "/keyboard")
 	{
@@ -312,54 +308,63 @@ void eventCB(const std::string &eventName, VRDataIndex *dataIndex) {
 		{
 			isRunning = false;
 		}
+		else if (val == "LEFT_down" || val == "LEFT_repeat")
+		{
+			horizAngle -= incAngle;
+		}else if (val == "RIGHT_down" || val == "RIGHT_repeat")
+		{
+			horizAngle += incAngle;
+		}else if (val == "UP_down" || val == "UP_repeat")
+		{
+			vertAngle -= incAngle;
+		}else if (val == "DOWN_down" || val == "DOWN_repeat")
+		{
+			vertAngle += incAngle;
+		}
+
+		if (horizAngle > 6.283185) horizAngle -= 6.283185;
+		if (horizAngle < 0.0) horizAngle += 6.283185;
+
+		if (vertAngle > 6.283185) vertAngle -= 6.283185;
+		if (vertAngle < 0.0) vertAngle += 6.283185;
+
+		cameraPos[0] = radius * cos(horizAngle) * cos(vertAngle);
+		cameraPos[1] = -radius * sin(vertAngle);
+		cameraPos[2] = radius * sin(horizAngle) * cos(vertAngle);
+
+		cameraAim[0] = cos(horizAngle) * sin(vertAngle);
+		cameraAim[1] = cos(vertAngle);
+		cameraAim[2] = sin(horizAngle) * sin(vertAngle);
+
 		cout << val << endl;
 	}
 
-  // The event handler's actions are here.
-  if (eventName.compare("/HeadAngleEvent") == 0) {
+	// The event handler's actions are here.
+	if (eventName.compare("/HeadAngleEvent") == 0) {
 
-    // If it's a head angle event, grab the two angles and calculate
-    // the eye position and direction.
-    horizAngle = dataIndex->getValue("/HeadAngleEvent/horizAngle");
-    vertAngle = dataIndex->getValue("/HeadAngleEvent/vertAngle");
+		// If it's a head angle event, grab the two angles and calculate
+		// the eye position and direction.
+		horizAngle = dataIndex->getValue("/HeadAngleEvent/horizAngle");
+		vertAngle = dataIndex->getValue("/HeadAngleEvent/vertAngle");
 
-    cameraPos[0] = radius * cos(horizAngle) * cos(vertAngle);
-    cameraPos[1] = -radius * sin(vertAngle);
-    cameraPos[2] = radius * sin(horizAngle) * cos(vertAngle);
+		cameraPos[0] = radius * cos(horizAngle) * cos(vertAngle);
+		cameraPos[1] = -radius * sin(vertAngle);
+		cameraPos[2] = radius * sin(horizAngle) * cos(vertAngle);
 
-    cameraAim[0] = cos(horizAngle) * sin(vertAngle);
-    cameraAim[1] = cos(vertAngle);
-    cameraAim[2] = sin(horizAngle) * sin(vertAngle);
+		cameraAim[0] = cos(horizAngle) * sin(vertAngle);
+		cameraAim[1] = cos(vertAngle);
+		cameraAim[2] = sin(horizAngle) * sin(vertAngle);
 
-  } else if (eventName.compare("/RadiusEvent") == 0) {
+	} else if (eventName.compare("/RadiusEvent") == 0) {
 
-    // This is a radius event.  Here's a condition for copying the event
-    // data, just to show we can do this.
-    if ((double)dataIndex->getValue("/RadiusEvent/radius") > 2.0) {
-      dataIndex->addData("/MVR/VRDisplayDevices/radius",
-                         (double)dataIndex->getValue("/RadiusEvent/radius"));
-    }
+		// This is a radius event.  Here's a condition for copying the event
+		// data, just to show we can do this.
+		if ((double)dataIndex->getValue("/RadiusEvent/radius") > 2.0) {
+			dataIndex->addData("/MVR/VRDisplayDevices/radius",
+					(double)dataIndex->getValue("/RadiusEvent/radius"));
+		}
 
-    // Perhaps there should be a
-    //    dataIndex->copyValue("/RadiusEvent/radius",
-    //                         "/MVR/VRDisplayDevices/radius");
+		radius = dataIndex->getValue("radius", "/MVR/VRDisplayDevices/" + MVR->getProcessName() + "/");
 
-    // Notice that the radius itself is set here.  Asking for a
-    // variable using a namespace allows there to be a default value
-    // defined at a higher level.  So if the specific display
-    // overrides the default radius value, it will not be affected by
-    // the event.
-
-    radius = dataIndex->getValue("radius", "/MVR/VRDisplayDevices/" + MVR->getProcessName() + "/");
-
-    //std::cout << "radius: " << radius << std::endl;
-
-    // The process name could be stored in the dataIndex instead of as
-    // a private class variable in VRMain.
-
-  }
-
-  // Print out the entire index.
-  //  std::cout << "Index Structure" << std::endl;
-  //  dataIndex->printStructure();
+	}
 }
