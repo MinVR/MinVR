f9ae56610d6dd2116147a157155641fe68e13f54
tsgouros <tsgouros@gmail.com>
2016-05-05 07:07:41 -0400

    added try/catch in MVRLookup

--- src/config/MVRLookup.cpp
+++ src/config/MVRLookup.cpp
@@ -31,85 +31,94 @@ int main(int argc, char **argv) {
   VRDataIndex *index = new VRDataIndex;
 
   // These will be extracted from argv.
-  std::string datumName;
   int nth = 0;
 
-  // Extracting...
-  switch(argc) {
+  try {
+  
+    // Extracting...
+    switch(argc) {
 
-  case 1:
-    HELPMESSAGE;
-    return EXIT_SUCCESS;
-    
-  case 2:
-    if (argv[1][0] == '-') {
+    case 1:
       HELPMESSAGE;
       return EXIT_SUCCESS;
+    
+    case 2:
+      if (argv[1][0] == '-') {
+        HELPMESSAGE;
+        return EXIT_SUCCESS;
+      }
+      throw std::runtime_error(std::string("Need a data name."));
+      break;
+
+    case 3:
+      // Don't really need to do anything here except avoid throwing error.
+      break;
+    
+    case 4:
+      // If there is a fourth arg, hope it's an integer.
+      sscanf(argv[3], "%d", &nth);
+      break;
+    
+    default:
+      throw std::runtime_error(std::string("Too many arguments"));
+      break;
     }
-    throw std::runtime_error(std::string("Need a data name."));
-    break;
 
-  case 4:
-    // If there is a fourth arg, hope it's an integer.
-    sscanf(argv[3], "%d", &nth);
+    index->processXMLFile(argv[1], std::string("/"));
 
-  case 3:
-    datumName = std::string(argv[2]);
-    break;
+    VRCORETYPE_ID tp = index->getType(argv[2]);
+
+    // std::cout << "datumName:" << datumName << " N:" << nth << " tp:" << tp << std::endl;
+
+    // Now print out the values, according to the data type.
+    switch(tp) {
+
+    case VRCORETYPE_INT: 
+      std::cout << (VRInt)index->getValue(argv[2]);
+      break;
+
+    case VRCORETYPE_DOUBLE: 
+      std::cout << (VRDouble)index->getValue(argv[2]);
+      break;
+
+    case VRCORETYPE_STRING:
+      std::cout << (VRString)index->getValue(argv[2]);
+      break;
+
+    case VRCORETYPE_INTARRAY: {
+      VRIntArray ia = index->getValue(argv[2]);
+      if (nth >= ia.size())
+        throw std::runtime_error(std::string("N too large for array"));
+      std::cout << ia[nth];
+      break;
+    }
     
-  default:
-    throw std::runtime_error(std::string("Too many arguments"));
-    break;
-  }
+    case VRCORETYPE_DOUBLEARRAY: {
+      VRDoubleArray ia = index->getValue(argv[2]);
+      if (nth >= ia.size())
+        throw std::runtime_error(std::string("N too large for array"));
+      std::cout << ia[nth];
+      break;
+    }
 
-  index->processXMLFile(argv[1], std::string("/"));
+    case VRCORETYPE_STRINGARRAY: {
+      VRStringArray ia = index->getValue(argv[2]);
+      if (nth >= ia.size())
+        throw std::runtime_error(std::string("N too large for array"));
+      std::cout << ia[nth];
+      break;
+    }
 
-  VRCORETYPE_ID tp = index->getType(argv[2]);
-
-  // std::cout << "datumName:" << datumName << " N:" << nth << " tp:" << tp << std::endl;
-
-  // Now print out the values, according to the data type.
-  switch(tp) {
-
-  case VRCORETYPE_INT: 
-    std::cout << (VRInt)index->getValue(argv[2]);
-    break;
-
-  case VRCORETYPE_DOUBLE: 
-    std::cout << (VRDouble)index->getValue(argv[2]);
-    break;
-
-  case VRCORETYPE_STRING:
-    std::cout << (VRString)index->getValue(argv[2]);
-    break;
-
-  case VRCORETYPE_INTARRAY: {
-    VRIntArray ia = index->getValue(argv[2]);
-    if (nth >= ia.size())
-      throw std::runtime_error(std::string("N too large for array"));
-    std::cout << ia[nth];
-    break;
-  }
+    case VRCORETYPE_CONTAINER:
+      std::cout << index->serialize(argv[2]);
+      break;
     
-  case VRCORETYPE_DOUBLEARRAY: {
-    VRDoubleArray ia = index->getValue(argv[2]);
-    if (nth >= ia.size())
-      throw std::runtime_error(std::string("N too large for array"));
-    std::cout << ia[nth];
-    break;
-  }
+    default:
+      throw std::runtime_error(std::string("Not supported for that type."));
+      break;
+    }
+  } catch (const std::exception& e) {
 
-  case VRCORETYPE_STRINGARRAY: {
-    VRStringArray ia = index->getValue(argv[2]);
-    if (nth >= ia.size())
-      throw std::runtime_error(std::string("N too large for array"));
-    std::cout << ia[nth];
-    break;
-  }
-    
-  default:
-    throw std::runtime_error(std::string("Not supported for that type."));
-    break;
+    std::cerr << "Oopsy-daisy: " << e.what() << std::endl;
   }
 }
-
