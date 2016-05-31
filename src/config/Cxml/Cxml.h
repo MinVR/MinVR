c31d21e512922089a34c040d23a6fdfb43cd4736
tsgouros <tsgouros@gmail.com>
2016-05-31 07:13:28 -0400

    added TODO list to Cxml

--- src/config/Cxml/Cxml.h
+++ src/config/Cxml/Cxml.h
@@ -20,3 +20,23 @@ class Cxml
     element* get_root_element();
 };
 #endif // CXML_H
+
+// TODO list -- Needed improvements to the Cxml parser.
+//
+//   - Limited entity support.  At least the basics: &, <, >, etc.
+//
+//   - Make a "printXML() method like printStructure(), for debugging.
+//
+//   - Support comments (again).  That is, restore the comment-processing
+//     calls, and persuade the data index code to ignore them.
+//
+//   - Real entity support, where they can be defined.
+//
+//   - CDATA support (maybe have this already?)
+//
+//   - Add hook for pre-processing with xsltproc, and validation to a schema.
+//
+//   - Tidying and better documentation.
+//
+//   - Better errors.
+//
