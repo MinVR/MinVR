234d60549903ec3b8aec286f3aa87eb6922e7b4f
Benjamin Knorlein <Benjamin_Knorlein@brown.edu>
2016-06-30 14:37:26 -0400

    Added Scalable displaynode plugin. Scalable is a proprietary library. Please contact Scalable Display Technologies of Cambridge, Massachusetts (scalabledisplay.com) for further information.

---
+++ plugins/Scalable/src/VRScalableNode.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright Regents of the University of Minnesota, 2015.  This software is released under the following license: http://opensource.org/licenses/GPL-2.0.
+ * Source code originally developed at the University of Minnesota Interactive Visualization Lab (http://ivlab.cs.umn.edu).
+ *
+ * Code author(s):
+ * 		Ben Knorlein
+ *
+ *  The software in this file uses a proprietary library from Scalable Display Technologies of Cambridge, Massachusetts (scalabledisplay.com).
+*/ 
+
+#include "VRScalableNode.h"
+#include "VRScalableHeaders.h"
+#include <math/VRMath.h>
+#include <cmath>
+#define DEGTORAD 3.14159/180.0
+
+namespace MinVR {
+
+VRScalableNode::VRScalableNode(const std::string &name, const std::string _meshName, double _near, double _far) : VRDisplayNode(name) , isInitialized(false), meshName(_meshName)
+		, near(_near), far(_far), isLeftEye(false){
+	
+}
+	
+VRScalableNode::~VRScalableNode() {
+	if(isInitialized){
+		EasyBlendSDK_Uninitialize(gMSDK );
+		delete gMSDK;
+	}
+}
+
+void
+VRScalableNode::render(VRDataIndex *renderState, VRRenderHandler *renderHandler)
+{
+	if(!isInitialized){
+		isInitialized = true;
+		gMSDK = new EasyBlendSDK_Mesh;		
+		EasyBlendSDKError msdkErr = EasyBlendSDK_Initialize(meshName.c_str(), gMSDK );
+		if ( msdkErr != EasyBlendSDK_ERR_S_OK )
+		{
+			std::cout << "Error on Init: " << EasyBlendSDK_GetErrorMessage(msdkErr) << std::endl;
+			std::cout << "File is: " << meshName <<std::endl;
+		}
+		
+		if(renderState->exists("Eye", "/") && renderState->getDatum("Eye", "/")->getValueString().compare("Right") == 0){
+			EasyBlendSDK_SetInputReadBuffer(gMSDK,  GL_BACK_RIGHT);
+			EasyBlendSDK_SetOutputDrawBuffer(gMSDK,  GL_BACK_RIGHT);
+			isLeftEye = false;
+		}else{
+			EasyBlendSDK_SetInputReadBuffer(gMSDK,  GL_BACK_LEFT);
+			EasyBlendSDK_SetOutputDrawBuffer(gMSDK,  GL_BACK_LEFT);	
+			isLeftEye = true;	
+		}
+	}
+
+	renderState->pushState();
+
+	VRPoint3 pe(0,0,0);
+	if (renderState->exists("LookAtMatrix","/")){
+		VRMatrix4 lookAtMatrix = renderState->getValue("LookAtMatrix","/");
+		VRMatrix4 head_frame = lookAtMatrix.inverse();
+		pe = VRPoint3(head_frame[3][0],head_frame[3][1], head_frame[3][2]);
+	}
+
+	EasyBlendSDK_SetEyepoint(gMSDK,pe[0],-pe[1],-pe[2]);
+	
+	EasyBlendSDK_Frustum Frustum = gMSDK->Frustum;
+
+	double l = near*tan(DEGTORAD*Frustum.LeftAngle);
+	double r = near*tan(DEGTORAD*Frustum.RightAngle);
+	double b = near*tan(DEGTORAD*Frustum.BottomAngle);
+	double t = near*tan(DEGTORAD*Frustum.TopAngle);
+	
+	VRMatrix4 projMat = VRMatrix4::projection(l, r, b, t, near, far);
+	renderState->addData("ProjectionMatrix", projMat);
+	
+	VRMatrix4 rotZ = VRMatrix4::rotationZ(DEGTORAD*Frustum.ViewAngleA);
+	VRMatrix4 rotY = VRMatrix4::rotationY(DEGTORAD*Frustum.ViewAngleB);
+	VRMatrix4 rotX = VRMatrix4::rotationX(DEGTORAD*(Frustum.ViewAngleC+90));
+	VRMatrix4 Mtrans = VRMatrix4::translation(VRPoint3(0, 0, 0) - pe);
+	VRMatrix4 viewMat = rotZ * rotY * rotX * Mtrans;
+
+	renderState->addData("ViewMatrix", viewMat);
+
+	VRDisplayNode::render(renderState, renderHandler);
+	
+	renderState->popState();
+}
+
+void 
+VRScalableNode::displayFinishedRendering(VRDataIndex *renderState) {
+	VRDisplayNode::displayFinishedRendering(renderState);
+	
+	glFinish();
+	EasyBlendSDK_TransformInputToOutput(gMSDK);
+}
+
+VRDisplayNode* VRScalableNode::create(VRMainInterface *vrMain, VRDataIndex *config,  const std::string &nameSpace) {
+	std::string nodeNameSpace = config->validateNameSpace(nameSpace);
+
+	std::string meshName = config->getValue("ScalableWarpFile", nodeNameSpace);
+	double nearClip = config->getValue("NearClip", nodeNameSpace);
+	double farClip = config->getValue("FarClip", nodeNameSpace);
+
+	VRDisplayNode *node = new VRScalableNode(nameSpace, meshName,nearClip,farClip);
+	
+	return node;
+}
+
+
+} // end namespace
---
+++ plugins/Scalable/src/VRScalableNode.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright Regents of the University of Minnesota, 2015.  This software is released under the following license: http://opensource.org/licenses/GPL-2.0.
+ * Source code originally developed at the University of Minnesota Interactive Visualization Lab (http://ivlab.cs.umn.edu).
+ *
+ * Code author(s):
+ * 		Ben Knorlein
+ * 
+ * The software in this file uses a proprietary library from Scalable Display Technologies of Cambridge, Massachusetts (scalabledisplay.com).
+ */
+
+#ifndef VRSCALABLENODE_H
+#define VRSCALABLENODE_H
+
+#include <display/VRDisplayNode.h>
+#include <main/VRFactory.h>
+#include <plugin/VRPlugin.h>
+
+//  The EasyBlendSDK is a proprietary library from Scalable Display Technologies of Cambridge, Massachusetts (scalabledisplay.com).
+#include "EasyBlendSDK.h"
+
+namespace MinVR {
+  
+/** OpenGL Implementation for the MinVR VRGraphicsToolkit abstraction.
+ */
+class VRScalableNode : public VRDisplayNode {
+public:
+	PLUGIN_API VRScalableNode(const std::string &name, const std::string meshName, double _near, double _far);
+	
+	PLUGIN_API virtual ~VRScalableNode();
+
+	PLUGIN_API virtual std::string getType() { return "VRScalableNode"; }
+
+	PLUGIN_API virtual void render(VRDataIndex *renderState, VRRenderHandler *renderHandler);
+	
+	PLUGIN_API virtual void displayFinishedRendering(VRDataIndex *renderState);
+	
+	//virtual void onVREvent(const std::string &eventName, VRDataIndex *eventData);
+	PLUGIN_API static VRDisplayNode* create(VRMainInterface *vrMain, VRDataIndex *config, const std::string &nameSpace);
+	
+private:
+	EasyBlendSDK_Mesh *gMSDK;
+	std::string meshName;
+	bool isInitialized;
+	bool isLeftEye;
+	double near;
+	double far;
+};
+
+} // end namespace
+
+#endif
