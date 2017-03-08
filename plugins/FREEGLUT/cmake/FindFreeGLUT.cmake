47072ddde7d0a08e982ed58d5f2e8440dbe464d7
tsgouros <tsgouros@gmail.com>
2017-03-08 07:25:54 -0500

    added hint for FreeGLUT build on Mac

--- plugins/FREEGLUT/cmake/FindFreeGLUT.cmake
+++ plugins/FREEGLUT/cmake/FindFreeGLUT.cmake
@@ -17,19 +17,22 @@
 
 # Look for the header file.
 FIND_PATH(FREEGLUT_INCLUDE_DIR 
-          NAMES GL/freeglut.h
-	  HINTS 
-	  ENV CPATH # For OSCAR modules at Brown/CCV
-	  )
+  NAMES GL/freeglut.h
+	HINTS 
+	ENV CPATH # For OSCAR modules at Brown/CCV
+  /usr/local/Cellar/freeglut/2.8.1/include # This is how it comes with Brew.
+	)
 
 # Look for the library.
 FIND_LIBRARY(FREEGLUT_LIBRARY 
-             NAMES glut
-	     HINTS
-	     ENV LD_LIBRARY_PATH # For OSCAR modules at Brown/CCV
-	     )
+  NAMES glut
+	HINTS
+	ENV LD_LIBRARY_PATH # For OSCAR modules at Brown/CCV
+  /usr/local/Cellar/freeglut/2.8.1/lib/libglut.3.dylib # Brew version.
+	)
 
-# Handle the QUIETLY and REQUIRED arguments and set FREEGLUT_FOUND to TRUE if all listed variables are TRUE.
+# Handle the QUIETLY and REQUIRED arguments and set FREEGLUT_FOUND to
+# TRUE if all listed variables are TRUE.
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
   FREEGLUT 
@@ -37,21 +40,17 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(
   FREEGLUT_LIBRARY 
   FREEGLUT_INCLUDE_DIR)
 
-# If you're using a Brew version of FreeGLUT, these might work.  Too specific
-# to add into a hint for FIND_LIBRARY or FIND_PATH.
-#set(FREEGLUT_INCLUDE_DIR /usr/local/Cellar/freeglut/2.8.1/include)
-#set(FREEGLUT_LIBRARY /usr/local/Cellar/freeglut/2.8.1/lib/libglut.3.dylib)
-
-message("FreeGLUT library: " ${FREEGLUT_LIBRARY})
-message("FreeGLUT include: " ${FREEGLUT_INCLUDE_DIR})
-
 # Copy the results to the output variables.
 IF(FREEGLUT_FOUND)
 	SET(FREEGLUT_LIBRARIES ${FREEGLUT_LIBRARY})
 	SET(FREEGLUT_INCLUDE_DIRS ${FREEGLUT_INCLUDE_DIR})
+  message("-- FreeGLUT includes: " ${FREEGLUT_INCLUDE_DIR})
+  message("-- FreeGLUT library:  " ${FREEGLUT_LIBRARY})
 ELSE(FREEGLUT_FOUND)
 	SET(FREEGLUT_LIBRARIES)
 	SET(FREEGLUT_INCLUDE_DIRS)
+  message("** FreeGLUT library not found.")
 ENDIF(FREEGLUT_FOUND)
 
 MARK_AS_ADVANCED(FREEGLUT_INCLUDE_DIRS FREEGLUT_LIBRARIES)
+
