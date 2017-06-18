25092c6e99299fe545a718dd740406b78410f728
tsgouros <tsgouros@gmail.com>
2017-06-18 13:59:13 -0400

    added network testing framework

---
+++ tests-batch/net/launchSwapClient.cpp
@@ -0,0 +1,32 @@
+#include "net/VRNetClient.h"
+
+// Program to launch one VRNetClient that connects to server.  Created
+// client executes syncSwapBuffersAcrossAllNodes.  The program is
+// intended to be executed by a forked child process in the network
+// tests.  We do this in a separate program because of limitations of
+// execl() that are made less limiting when it simply starts up a new
+// binary.
+int main(int argc, char* argv[]) {
+
+  MinVR::VRNetClient client = MinVR::VRNetClient("localhost", "3490");
+
+  // Engineer a small sleep sometimes.
+  srand(time(NULL));
+  sleep(3 + (random() % 4));
+
+  // Get the client number from the argv list and the process ID.
+  std::stringstream clientIDstring;
+  clientIDstring << "client " << std::string(argv[1]) << ", pid = " << getpid();
+
+  // Make the swap buffers request.
+	std::cout << "launchSwapClient: sync swap buffers request from "
+            << clientIDstring.str() << std::endl;
+
+  for (int i = 0; i < 10; i++) {
+    client.syncSwapBuffersAcrossAllNodes();
+    std::cout << "launchSwapClient: swap request " << i << " received, "
+            << clientIDstring.str() << std::endl;
+  }
+
+	exit(0);
+}
