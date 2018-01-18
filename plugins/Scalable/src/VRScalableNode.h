55d44ddce447971e402fb72917bcff560046e5ad
tsgouros <tsgouros@gmail.com>
2018-01-18 15:42:16 -0500

    added const to getType()

--- plugins/Scalable/src/VRScalableNode.h
+++ plugins/Scalable/src/VRScalableNode.h
@@ -4,7 +4,7 @@
  *
  * Code author(s):
  * 		Ben Knorlein
- * 
+ *
  * The software in this file uses a proprietary library from Scalable Display Technologies of Cambridge, Massachusetts (scalabledisplay.com).
  */
 
@@ -19,24 +19,24 @@
 #include "EasyBlendSDK.h"
 
 namespace MinVR {
-  
+
 /** OpenGL Implementation for the MinVR VRGraphicsToolkit abstraction.
  */
 class VRScalableNode : public VRDisplayNode {
 public:
 	PLUGIN_API VRScalableNode(const std::string &name, const std::string meshName, double _near, double _far);
-	
+
 	PLUGIN_API virtual ~VRScalableNode();
 
-	PLUGIN_API virtual std::string getType() { return "VRScalableNode"; }
+	PLUGIN_API virtual std::string getType() const { return "VRScalableNode"; }
 
 	PLUGIN_API virtual void render(VRDataIndex *renderState, VRRenderHandler *renderHandler);
-	
+
 	PLUGIN_API virtual void displayFinishedRendering(VRDataIndex *renderState);
-	
+
 	//virtual void onVREvent(const std::string &eventName, VRDataIndex *eventData);
 	PLUGIN_API static VRDisplayNode* create(VRMainInterface *vrMain, VRDataIndex *config, const std::string &nameSpace);
-	
+
 private:
 	EasyBlendSDK_Mesh *gMSDK;
 	std::string meshName;
