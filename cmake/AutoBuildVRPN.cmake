7d66d9616bdbcc3760e79b92dce7bca0dca9dccd
Daniel Keefe <dfk@umn.edu>
2019-08-21 09:06:44 -0500

    simplifying netserver code, making sure INADDR_ANY is set

--- cmake/AutoBuildVRPN.cmake
+++ cmake/AutoBuildVRPN.cmake
@@ -38,6 +38,7 @@ macro(AutoBuild_use_package_VRPN YOUR_TARGET INTERFACE_PUBLIC_OR_PRIVATE)
                 -DVRPN_BUILD_PYTHON=OFF
                 -DVRPN_USE_DIRECTINPUT=OFF
                 -DVRPN_USE_DIRECTSHOW=OFF
+                -DCMAKE_CXX_STANDARD=17
             )
 
             if (MSVC)
